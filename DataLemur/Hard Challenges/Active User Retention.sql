4b74ad7c33c73e0fb1b69c9627b13041d3af7f33
Jack Didier <110349685+jackdda@users.noreply.github.com>
2023-02-13 14:14:45 -0800

    Add files via upload

---
+++ DataLemur/Hard Challenges/Active User Retention.sql
@@ -0,0 +1,22 @@
+/* 
+Assume you have the table below containing information on Facebook user actions. Write a query to obtain the active user retention in July 2022. Output the month (in numerical format 1, 2, 3) and the number of monthly active users (MAUs).
+
+Hint: An active user is a user who has user action ("sign-in", "like", or "comment") in the current month and last month.
+
+user_actions Table:
+Column Name		Type
+user_id			integer
+event_id		integer
+event_type		string ("sign-in, "like", "comment")
+event_date		datetime
+*/
+
+SELECT EXTRACT(MONTH FROM event_date) as month, COUNT(DISTINCT user_id) as monthly_active_users
+FROM user_actions
+WHERE EXTRACT(MONTH FROM event_date) = 7 
+  AND user_id IN (
+    SELECT user_id
+    FROM user_actions
+    WHERE EXTRACT(MONTH FROM event_date) = 6
+    )
+GROUP BY EXTRACT(MONTH FROM event_date)
---
+++ DataLemur/Hard Challenges/Y-on-Y Growth Rate.sql
@@ -0,0 +1,25 @@
+/* 
+Assume you are given the table below containing information on user transactions for particular products. Write a query to obtain the year-on-year growth rate for the total spend of each product for each year.
+
+Output the year (in ascending order) partitioned by product id, current year's spend, previous year's spend and year-on-year growth rate (percentage rounded to 2 decimal places).
+
+user_transactions Table:
+Column Name			Type
+transaction_id		integer
+product_id			integer
+spend				decimal
+transaction_date	datetime
+*/
+
+SELECT year
+    , product_id
+    , curr_year_spend
+    , prev_year_spend
+    , ROUND((curr_year_spend-prev_year_spend)/prev_year_spend * 100, 2) as yoy_rate
+FROM (
+  SELECT EXTRACT(Year from transaction_date) as year
+    , product_id
+    , spend as curr_year_spend
+    , LAG(spend) OVER(PARTITION BY product_id ORDER BY EXTRACT(Year from transaction_date)) as prev_year_spend
+  FROM user_transactions
+  ) as lag
