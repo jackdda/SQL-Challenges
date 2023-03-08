2348dd8b1609d447ffa3451498d73c3aea0f1ccf
Jack Didier <110349685+jackdda@users.noreply.github.com>
2023-03-07 20:22:45 -0800

    Add files via upload

--- Data In Motion - Weekly Challenges/Week 29.sql
+++ Data In Motion - Weekly Challenges/Week 29.sql
@@ -22,27 +22,3 @@ SELECT user_id
 FROM tweets
 GROUP BY user_id, tweet_date
 ORDER BY user_id, tweet_date
-
-/* You are given the tables below containing information on Robinhood trades and
-users.
-Write a query to list the top three cities that have the most completed trade orders in descending order.
-Output the city and number of orders.
-
-trades Table:                                               users Table:
-Column Name     Type                                        Column Name Type
-order_id        integer                                     user_id     integer
-user_id         integer                                     city        string
-price           decimal                                     email       string
-quantity        integer                                     signup_date datetime
-status          string('Completed', 'Cancelled')
-timestamp       datetime
-*/
-
-SELECT city, COUNT(*) AS total_orders
-FROM trades t
-JOIN users u
-    ON t.user_id = u.user_id
-WHERE status = 'Completed'
-GROUP BY city
-ORDER BY total_orders DESC 
-LIMIT 3
--- Data In Motion - Weekly Challenges/Week 30.sql
+++ Data In Motion - Weekly Challenges/Week 30.sql
@@ -1,34 +1,4 @@
-/* Solution #1:
-Given the reviews table, write a query to get the average stars for each product every month.
-
-The output should include the month in numerical value, product id, and average star rating rounded to two decimal places. Sort the output based on month followed by the product id.
-
-reviews Table:
-Column Name	Type
-review_id	integer
-user_id		integer
-submit_date	datetime
-product_id	integer
-stars		integer (1-5)
-
-review_id	user_id		submit_date				product_id		stars
-6171		123			06/08/2022 00:00:00		50001			4
-7802		265			06/10/2022 00:00:00		69852			4
-5293		362			06/18/2022 00:00:00		50001			3
-6352		192			07/26/2022 00:00:00		69852			3
-4517		981			07/05/2022 00:00:00		69852			2
-*/
-
-
-SELECT EXTRACT(MONTH FROM submit_date) AS month
-  , product_id AS product
-  , ROUND(AVG(stars), 2) AS avg_stars
-FROM review
-GROUP BY EXTRACT(MONTH FROM submit_date)
-  , product_id
-ORDER BY month, product_id;
-
-/* Solution #2
+/*
 Assume you are given the table containing measurement values obtained from a Google sensor over several days. Measurements are taken several times within a given day.
 
 Write a query to obtain the sum of the odd-numbered and even-numbered measurements on a particular day, in two different columns. Refer to the Example Output below for the output format.
@@ -37,19 +7,11 @@ Definition:
 1st, 3rd, and 5th measurements taken within a day are considered odd-numbered measurements and the 2nd, 4th, and 6th measurements are even-numbered measurements.
 
 measurements Table:
-Column Name	Type
-measurement_id		integer
+Column Name     	Type
+measurement_id	    integer
 measurement_value	decimal
 measurement_time	datetime
-
-measurement_id	measurement_value	measurement_time
-131233			1109.51				07/10/2022 09:00:00
-135211			1662.74				07/10/2022 11:00:00
-523542			1246.24				07/10/2022 13:15:00
-143562			1124.50				07/11/2022 15:00:00
-346462			1234.14				07/11/2022 16:45:00
 */
-
 WITH Numbered AS (
   SELECT *
   , (ROW_NUMBER() OVER(PARTITION BY EXTRACT(DAY FROM measurement_time) ORDER BY measurement_time))%2 as even_odd
