b1bde63bb8ab601d178d33b256aa1f360f432aa5
Jack Didier <110349685+jackdda@users.noreply.github.com>
2023-11-28 13:16:05 -0800

    Add files via upload

---
+++ DataLemur/Easy Challenges/Data Science Skills.sql
@@ -0,0 +1,12 @@
+/*Given a table of candidates and their skills, you're tasked with finding the 
+candidates best suited for an open Data Science job. You want to find candidates 
+who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for 
+the job. Sort the output by candidate ID in ascending order. */
+
+SELECT DISTINCT candidate_id
+FROM candidates
+WHERE candidate_id IN (SELECT candidate_id from candidates where skill = 'Python')
+  AND candidate_id IN (SELECT candidate_id from candidates where skill = 'Tableau')
+  AND candidate_id IN (SELECT candidate_id from candidates where skill = 'PostgreSQL')
---
+++ DataLemur/Easy Challenges/Second Day Confirmation.sql
@@ -0,0 +1,14 @@
+/*Assume you're given tables with information about TikTok user 
+sign-ups and confirmations through email and text. New users on 
+TikTok sign up using their email addresses, and upon sign-up, 
+each user receives a text message confirmation to activate their account.
+
+Write a query to display the user IDs of those who did not confirm 
+their sign-up on the first day, but confirmed on the second day. */
+
+SELECT user_id
+FROM emails e
+JOIN texts t 
+  ON e.email_id = t.email_id
+WHERE action_date::DATE - signup_date::DATE = 1
+  AND signup_action = 'Confirmed'
