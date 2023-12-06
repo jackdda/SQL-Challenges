628109960f4ca277e84d8f8c44069bd342b90f5d
Jack Didier <110349685+jackdda@users.noreply.github.com>
2023-12-06 10:50:57 -0800

    Add files via upload

---
+++ DataLemur/Easy Challenges/Pharmacy Analytics (Part 3).sql
@@ -0,0 +1,15 @@
+/* CVS Health wants to gain a clearer understanding of its pharmacy sales and the
+performance of various products.
+
+Write a query to calculate the total drug sales for each manufacturer. Round the 
+answer to the nearest million and report your results in descending order of total 
+sales. In case of any duplicates, sort them alphabetically by the manufacturer name.
+
+Since this data will be displayed on a dashboard viewed by business stakeholders, 
+please format your results as follows: "$36 million". */
+
+SELECT manufacturer
+  , CONCAT('$', ROUND(SUM(total_sales)/1000000, 0), ' million') as sales_mil
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM(total_sales) DESC
